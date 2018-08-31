c616bf5d1ee81beefb83d9c1c6b86ff61b021d47
Eric Nichols <epnichols@gmail.com>
2018-08-31 01:50:14 -0700

    Command-line version of continuation generation.

---
+++ code/data_processing.py
@@ -0,0 +1,370 @@
+
+from collections import namedtuple
+import copy
+from enum import Enum
+import json
+import os
+
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# # Utilities
+
+# ## Constants
+
+# In[2]:
+
+
+SIG_DIGITS = 4
+QUANTIZATION = 12  # smallest unit is 1/12 of a beat
+MAX_EVENT_BEATS = 4
+
+MIDI_MIN = 21
+MIDI_MAX = 108
+
+MAX_EVENT_SUBBEATS = QUANTIZATION * MAX_EVENT_BEATS
+
+MIDI_LEN = MIDI_MAX - MIDI_MIN + 1  # 88 keys
+
+# One-hot vector lengths
+NUM_COMMAND_CLASSES = 3
+NUM_MIDI_CLASSES = MIDI_LEN + 1                # + 1 for "0" case
+NUM_DURATION_CLASSES = MAX_EVENT_SUBBEATS + 1  # + 1 for "0" case
+
+# Start of range is inclusive, end of range is exclusive. 
+COMMAND_VEC_RANGE = (0, NUM_COMMAND_CLASSES)
+MIDI_VEC_RANGE = (COMMAND_VEC_RANGE[1], COMMAND_VEC_RANGE[1] + NUM_MIDI_CLASSES)
+DURATION_VEC_RANGE = (MIDI_VEC_RANGE[1], MIDI_VEC_RANGE[1] + NUM_DURATION_CLASSES)
+VEC_LENGTH = DURATION_VEC_RANGE[1]
+
+INPUT_NOTES = 30
+OUTPUT_NOTES = 10
+SLIDING_WINDOW_NOTES = 5
+
+INPUT_TIMESTEPS = 4 * INPUT_NOTES
+OUTPUT_TIMESTEPS = 4 * OUTPUT_NOTES
+SLIDING_WINDOW_TIMESTEPS = 4 * SLIDING_WINDOW_NOTES
+
+
+## Key
+
+
+class KeyQuality(Enum):
+    Major = 0,
+    Minor = 1
+
+
+STR_TO_PITCH_CLASS = {
+    'C': 0,
+    'B#': 0,
+    'C#': 1,
+    'DB': 1,
+    'D': 2,
+    'D#': 3,
+    'EB': 3,
+    'E': 4,
+    'FB': 4,
+    'F': 5,
+    'F#': 6,
+    'GB': 6,
+    'G': 7,
+    'G#': 8,
+    'AB': 8,
+    'A': 9,
+    'A#': 10,
+    'BB': 10,
+    'B': 11,
+    'CB': 11
+}
+
+STR_TO_MODE = {
+    'MAJOR': KeyQuality.Major,
+    'MINOR': KeyQuality.Minor
+}
+
+def parse_key_string(str_key):
+    """Returns a tuple of root pitch class (int, 0=C to 11=B) and KeyQuality."""
+    tokens = str_key.strip().upper().split(' ')
+    if len(tokens) != 2:
+        raise Exception("Can't parse key: %s" % str_key)
+    pitch_str, mode_str = tokens
+    pc = STR_TO_PITCH_CLASS[pitch_str]
+    mode = STR_TO_MODE[mode_str]
+    return pc, mode    
+
+
+# Time
+
+def get_timeshift_set(score):
+    s = set()
+    for _, event_type, _, time_delta in score:
+        if event_type == ScoreEventType.TimeShift:
+            s.add(time_delta)
+    return s
+
+
+class Duration():
+    def __init__(self, beats, subbeats, quantization=QUANTIZATION):
+        self.beats = beats
+        self.subbeats = subbeats
+        self.quantization = quantization
+    
+    def total_subbeats(self):
+        return self.beats * self.quantization + self.subbeats
+    
+    def subtract_subbeats(self, subbeats):
+        """Modify the duration by subtracting the given # of subbeats."""
+        # Find the total # of full beats to subtract.
+        beats_to_subtract = subbeats // QUANTIZATION    # int part
+        subbeats_to_subtract = subbeats % QUANTIZATION  # fractional part
+        
+        self.beats -= beats_to_subtract
+        self.subbeats -= subbeats_to_subtract         # might be negative now
+        
+        # Borrow 1 from beats, just like in by-hand subtraction
+        if self.subbeats < 0:
+            self.subbeats += QUANTIZATION
+            self.beats -= 1
+        
+    @staticmethod
+    def MakeDuration(float_duration, quantization=QUANTIZATION):
+        beats = int(float_duration)
+        subbeats = round((float_duration % 1) * quantization)
+        return Duration(beats, subbeats, quantization)
+    
+    def __repr__(self):
+        return 'Duration(%d,%d,%d)' % (self.beats, self.subbeats, self.quantization)
+
+
+# Score Types
+
+class ScoreEventType(Enum):
+    Undefined = -1
+    NoteOff = 0
+    NoteOn = 1
+    TimeShift = 2
+    
+# In neural net, representation will be: [COMMAND, OPT_MIDI, OPT_TIME_DELTA]
+# That is, 3 separate one-hot-encoding blocks, with N/A value possible for MIDI.
+# ex:
+# [NoteOn, MIDI, 0], [NoteOff, MIDI, 0], [TimeShift, N/A, time_delta]
+
+# MIDI is an int, 0 for N/A or 1-88 representing piano keys 21 to 108
+# time_delta will be quantized 1/12 of quarter notes. Duration can be from 0-48 12/th notes.
+#    Longer durations will be represented with multiple successive time_shift commands.
+    
+# time is a float, mostly for output use; not fed into neural net.
+# time_delta is a quantized Duration object.
+
+ScoreEvent = namedtuple('ScoreEvent', ['time', 'type', 'midi', 'time_delta'])
+
+
+def convert_df_to_score(df): # Monophonic
+    """The df is an input csv with time, midi, and duration.
+    Output is a list of ScoreEvents."""
+    
+    # Fix durations that are too long and overlap later notes. This is supposed to be
+    # monophonic data, but it isn't always.
+    for i, row in df.iterrows():
+        if i >= len(df) - 1: # skip last row
+            break
+        time = row['time']
+        next_time = df.loc[i+1, 'time']
+        if time + row['duration'] > next_time:
+            df.loc[i, 'duration']= next_time - time
+        
+    events = []
+    prev_time = None
+    for _, row in df.iterrows():
+        time = round(row['time'], SIG_DIGITS)
+        midi = int(row['midi'])
+        duration = round(row['duration'], SIG_DIGITS)
+        
+        if prev_time is None:
+            prev_time = time
+        
+        delta_time = round(time - prev_time, SIG_DIGITS)
+        
+        # Quantize time to beats and subbeats.
+        time_quantized = Duration.MakeDuration(time)
+        duration_quantized = Duration.MakeDuration(duration)
+        delta_time_quantized = Duration.MakeDuration(delta_time)
+        
+        # Create events.
+        #if delta_time_quantized.total_subbeats() > 0:
+        # TimeShift.
+        events.append(ScoreEvent(prev_time, ScoreEventType.TimeShift, None, delta_time_quantized))
+        
+        # NoteOn
+        events.append(ScoreEvent(time, ScoreEventType.NoteOn, midi, None))
+        
+        # TimeShift for duration of note
+        # DOESN'T WORK if duration is too long for next note. 
+        # Preprocessing above fixes durations.
+        events.append(ScoreEvent(time, ScoreEventType.TimeShift, None, duration_quantized))
+        time += duration
+        
+        # NoteOff
+        events.append(ScoreEvent(time, ScoreEventType.NoteOff, midi, None))
+        
+        prev_time = time
+    
+    return events            
+
+
+# In[10]:
+
+
+def score_event_to_nnet_input_list(ev):
+    # Output is a list of numpy arrays: 3 one-hot encodings, concatenated.
+    # List will usually be 1-element long, but must be multiple for long durations.
+    # numpy array has 3 one-hot sections (command, midi, duration)
+    # All 3 are concatenated together into ndarray: [command...midi...duration]
+    
+    _, event_type, midi, time_delta_orig = ev
+    time_delta = copy.copy(time_delta_orig)
+    #print(ev)
+    result = []
+    
+    command_vec = to_categorical(event_type.value, num_classes=NUM_COMMAND_CLASSES)
+    
+    if not midi:
+        midi = 0
+    else:
+        midi = midi - MIDI_MIN + 1
+    
+    midi_vec = to_categorical(midi, num_classes=NUM_MIDI_CLASSES)  # +1 for the 0 case
+    
+    if not time_delta:
+        time_delta = Duration(0, 0)
+    
+    # Handle long duration events: repeat them.
+    while time_delta.total_subbeats() > MAX_EVENT_SUBBEATS:
+        duration_vec = to_categorical(MAX_EVENT_SUBBEATS, num_classes=NUM_DURATION_CLASSES)
+        total_vec = np.concatenate((command_vec, midi_vec, duration_vec))
+        result.append(total_vec)
+        time_delta.subtract_subbeats(MAX_EVENT_SUBBEATS)
+        
+    # Regular duration event.
+    duration_vec = to_categorical(time_delta.total_subbeats(), num_classes=NUM_DURATION_CLASSES)
+    result.append(np.concatenate((command_vec, midi_vec, duration_vec)))
+    
+    return result
+
+
+def score_to_array(score):
+    arrays_list = []
+    for ev in score:
+        arrays_list.append(np.array(score_event_to_nnet_input_list(ev)))
+    return np.vstack(arrays_list)
+
+
+def plot_matrix(x):
+    plt.figure(figsize=(20,10))
+    plt.imshow(x, origin='lower')
+    plt.show()
+
+def plot_score(score):
+    plot_matrix(score_to_array(score).T)
+
+
+# MIDI
+
+def get_midi_set(score):
+    s = set()
+    for _, event_type, midi, _ in score:
+        if event_type == ScoreEventType.NoteOn:
+            s.add(midi)
+    return s
+
+
+# File parsing
+
+size = 'large'   # 'small', medium', 'large'
+DATA_PATH = 'data/PPDD-Jul2018_sym_mono_%s/PPDD-Jul2018_sym_mono_%s' % (size, size)
+
+
+DESCRIPTOR_PATH = os.path.join(DATA_PATH, 'descriptor')       # .json files
+PRIME_CSV_PATH = os.path.join(DATA_PATH, 'prime_csv')         # .csv
+CONT_FOIL_CSV_PATH = os.path.join(DATA_PATH, 'cont_foil_csv') # .csv
+CONT_TRUE_CSV_PATH = os.path.join(DATA_PATH, 'cont_true_csv') # .csv
+
+
+def get_file_list(path, ext):
+    """Returns a list of all files in the given path with the given string ending 'ext'."""
+    return [f for f in os.listdir(path) if 
+            os.path.isfile(os.path.join(path, f)) and f.endswith(ext)]
+
+#descriptor_files = get_file_list(DESCRIPTOR_PATH, 'json')
+
+
+# Read Dataset
+
+all_scores = []
+
+def read_dataset():
+    # all_durations = set()
+    #all_midi = set()
+
+    for i, json_file in enumerate(descriptor_files):
+        if i % 1000 == 0:
+            print(i)
+        guid = os.path.splitext(json_file)[0]
+        #print('Processing file %d: %s' % (i, guid))
+        prime_csv_file = os.path.join(PRIME_CSV_PATH, guid) + '.csv'
+
+        df_prime = pd.read_csv(prime_csv_file, header=None, 
+                               names=['time', 'midi', 'mpn', 'duration', 'channel'])
+        score = convert_df_to_score(df_prime)
+        all_scores.append(score)
+
+        # Used this code to analyze dataset.
+        # Result: quantization is to the 1/12 of a beat.
+        #all_durations.update(get_timeshift_set(score))
+
+        # Result: MIDI notes from 21-108 in use
+        #all_midi.update(get_midi_set(score))
+
+        with open(os.path.join(DESCRIPTOR_PATH, descriptor_files[i])) as f:
+            j = json.load(f)
+            id_lakh = j['idLakh']
+            bpm = j['bpm']
+            time_sig_numerator, time_sig_denominator = j.get('timeSignature', [4, 4])
+            key_estimate = j['keyEstimate']
+
+            #print (guid, id_lakh, bpm, time_sig_numerator, time_sig_denominator, key_estimate)
+    print('Done!')
+    return [score_to_array(s) for s in all_scores]
+
+# Training Examples
+
+
+def get_example(matrix, start_row, input_timesteps=INPUT_TIMESTEPS, output_timesteps=OUTPUT_TIMESTEPS):
+    """Returns a pair of input, output ndarrays. Input starts at start_row and has the given input length.
+    Output starts at next timestep and has the given output length."""
+    # Make sure there are enough time steps remaining.
+    if len(matrix) < start_row + input_timesteps + output_timesteps:
+        raise Exception('Not enough rows to get example.')
+    input_ex = matrix[start_row : start_row + input_timesteps]
+    output_ex = matrix[start_row + input_timesteps : start_row+input_timesteps+output_timesteps]
+    return (input_ex, output_ex)
+
+
+def get_examples_for_song(matrix, input_timesteps=INPUT_TIMESTEPS, output_timesteps=OUTPUT_TIMESTEPS, 
+                          sliding_window_hop_length=SLIDING_WINDOW_TIMESTEPS):
+    """Returns tuple of (array of input examples, array of output examples), generated via a sliding window on
+    the input matrix. For example: output might be two ndarrays of shapes (277, 120, 141), (277, 40, 141).
+    Dimensions are (examples, timesteps, features)
+    """
+    input_examples = []
+    output_examples = []
+    for i in range(len(matrix) - input_timesteps - output_timesteps + 1):
+        x, y = get_example(matrix, i, input_timesteps, output_timesteps)
+        input_examples.append(x)
+        output_examples.append(y)
+    if not input_examples:
+        return None
+    return np.stack(input_examples), np.stack(output_examples)
---
+++ code/generate_continuation_mono.py
@@ -0,0 +1,333 @@
+
+from functools import partial
+import glob
+
+from keras.layers import Input
+from keras.models import Model, load_model
+import numpy as np
+from sklearn.utils import shuffle
+import sys
+
+from Dataset import convert_df_to_score, score_event_to_nnet_input_list
+from Dataset import get_file_list, read_dataset, get_examples_for_song
+
+SIG_FIGS = 5
+SIG_DIGITS = 4
+QUANTIZATION = 12  # smallest unit is 1/12 of a beat
+MAX_EVENT_BEATS = 4
+
+MIDI_MIN = 21
+MIDI_MAX = 108
+
+MAX_EVENT_SUBBEATS = QUANTIZATION * MAX_EVENT_BEATS
+
+MIDI_LEN = MIDI_MAX - MIDI_MIN + 1  # 88 keys
+
+# Adds a start command node for the Y data in timestep 0, to start off decoder. Also expands feature vector length.
+ADD_START = True  # Set to False for Attention, True for basic seq2seq
+USE_TEACHER_FORCING = True  # False for Attention, True for basic seq2seq
+
+# One-hot vector lengths
+NUM_COMMAND_CLASSES = 3
+if ADD_START:
+    NUM_COMMAND_CLASSES += 1
+
+NUM_MIDI_CLASSES = MIDI_LEN + 1  # + 1 for "0" case
+NUM_DURATION_CLASSES = MAX_EVENT_SUBBEATS + 1  # + 1 for "0" case
+
+# Start of range is inclusive, end of range is exclusive.
+COMMAND_VEC_RANGE = (0, NUM_COMMAND_CLASSES)
+MIDI_VEC_RANGE = (
+COMMAND_VEC_RANGE[1], COMMAND_VEC_RANGE[1] + NUM_MIDI_CLASSES)
+DURATION_VEC_RANGE = (
+MIDI_VEC_RANGE[1], MIDI_VEC_RANGE[1] + NUM_DURATION_CLASSES)
+VEC_LENGTH = DURATION_VEC_RANGE[1]
+
+# Y values have an extra "start" command option. X values don't.
+VEC_LENGTH_X = VEC_LENGTH
+if ADD_START:
+    VEC_LENGTH_X -= 1
+
+INPUT_NOTES = 30
+OUTPUT_NOTES = 10   # Used for training
+
+MAX_OUTPUT_STEPS = 100
+OUTPUT_BEATS = 10  # Used for generation
+
+
+INPUT_TIMESTEPS = 4 * INPUT_NOTES
+OUTPUT_TIMESTEPS = 4 * OUTPUT_NOTES
+
+# y values have an extra timestep if adding start symbol
+if ADD_START:
+    OUTPUT_TIMESTEPS += 1
+
+pc_to_degree_flat_key = [0, 1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 6]
+pc_to_degree_sharp_key = [0, 0, 1, 1, 2, 3, 3, 4, 4, 5, 5, 6]
+
+
+
+TEST_FILES = sorted(glob.glob(TEST_PATH))
+
+
+def example_generator(teacher_forcing=USE_TEACHER_FORCING):
+    files = TEST_FILES
+
+    while True:
+        for file in shuffle(files):
+            # print('new FILE!')
+            x, y = load_dataset(file)
+
+            if ADD_START:
+                # Insert room for START data as feature 0
+                y = np.concatenate((np.expand_dims(
+                    np.array([[0] * (OUTPUT_TIMESTEPS - 1)] * len(y)), axis=2),
+                                    y), axis=2)
+
+                # Insert START symbol (1 and then 0s) before each example in y data
+                z = np.array([[[1] + [0] * (VEC_LENGTH - 1)]] * len(y))
+                y = np.hstack((z, y))
+
+            # Now create the desired data format for the encoder/decoder pieces:
+            #                INPUTS                             OUTPUTS (command, midi, duration one-hots)
+            # ([encoder_input_data, decoder_input_data], [decoder_target_data1, decoder_target_data2, decoder_target_data3])
+
+            y_shifted = y[:, 1:, :] if ADD_START else y
+            decoder_target_data1 = y_shifted[:, :,
+                                   COMMAND_VEC_RANGE[0]:COMMAND_VEC_RANGE[1]]
+            decoder_target_data2 = y_shifted[:, :,
+                                   MIDI_VEC_RANGE[0]:MIDI_VEC_RANGE[1]]
+            decoder_target_data3 = y_shifted[:, :,
+                                   DURATION_VEC_RANGE[0]:DURATION_VEC_RANGE[1]]
+
+            for i in range(0, len(x), BATCH_SIZE):
+                # print('new batch')
+                j = i + BATCH_SIZE
+                if teacher_forcing:
+                    yield ([x[i: j], y[i: j, :-1]],
+                           [decoder_target_data1[i: j],
+                            decoder_target_data2[i: j],
+                            decoder_target_data3[i: j]])
+                else:
+                    yield (x[i: j],
+                           [decoder_target_data1[i: j],
+                            decoder_target_data2[i: j],
+                            decoder_target_data3[i: j]])
+
+validation_generator = example_generator()
+
+# Utils
+
+
+# Load from disk; reconstruct inference model
+# Returns a function that performs inference on a given sequence.
+def load_model():
+    model = load_model('s2s_mono_continuation.h5')
+
+    encoder_inputs = model.get_layer(name='encoder_input').input
+    encoder_lstm_1 = model.get_layer(name='encoder_lstm_1')
+    encoder_states1 = encoder_lstm_1.output[1:3]
+    encoder_lstm_2 = model.get_layer(name='encoder_lstm_2')
+    encoder_states2 = encoder_lstm_2.output[1:3]
+
+    decoder_inputs = model.get_layer(name='decoder_lstm_1').input[0]
+
+    decoder_lstm1 = model.get_layer(name='decoder_lstm_1')
+    decoder_lstm2 = model.get_layer(name='decoder_lstm_2')
+    decoder_dense1 = model.get_layer(name='decoder_output_command')
+    decoder_dense2 = model.get_layer(name='decoder_output_midi')
+    decoder_dense3 = model.get_layer(name='decoder_output_duration')
+
+    # Run Model
+
+    # Define sampling models
+    encoder_model1 = Model(encoder_inputs, encoder_states1)
+    encoder_model1.summary()
+
+    encoder_model2 = Model(encoder_inputs, encoder_states2)
+    encoder_model2.summary()
+
+    decoder_state_input_h1 = Input(shape=(NUM_LSTM_NODES,),
+                                   name='inference_decoder_h1')
+    decoder_state_input_c1 = Input(shape=(NUM_LSTM_NODES,),
+                                   name='inference_decoder_c1')
+    decoder_state_input_h2 = Input(shape=(NUM_LSTM_NODES,),
+                                   name='inference_decoder_h2')
+    decoder_state_input_c2 = Input(shape=(NUM_LSTM_NODES,),
+                                   name='inference_decoder_c2')
+    decoder_states_inputs1 = [decoder_state_input_h1, decoder_state_input_c1]
+    decoder_states_inputs2 = [decoder_state_input_h2, decoder_state_input_c2]
+
+    zz1 = decoder_lstm1(decoder_inputs, initial_state=decoder_states_inputs1)
+    decoder_outputs1new, decoder_state_h1, decoder_state_c1 = zz1
+
+    zz2 = decoder_lstm2(decoder_outputs1new, initial_state=decoder_states_inputs2)
+
+    decoder_outputs2new, decoder_state_h2, decoder_state_c2 = zz2
+
+    decoder_states1 = [decoder_state_h1, decoder_state_c1]
+    decoder_states2 = [decoder_state_h2, decoder_state_c2]
+    decoder_outputs_final1 = decoder_dense1(decoder_outputs2new)
+    decoder_outputs_final2 = decoder_dense2(decoder_outputs2new)
+    decoder_outputs_final3 = decoder_dense3(decoder_outputs2new)
+
+    decoder_model = Model(
+        [decoder_inputs] + decoder_states_inputs1 + decoder_states_inputs2,
+        [decoder_outputs_final1, decoder_outputs_final2,
+         decoder_outputs_final3] + decoder_states1 + decoder_states2)
+
+
+    def seq2seq_from_models(encoder_model1, encoder_model2, decoder_model,
+                            input_seq):
+        # Encode the input as state vectors.
+        h1, c1 = encoder_model1.predict(input_seq)
+        states_value1 = [h1, c1]
+        h2, c2 = encoder_model2.predict(input_seq)
+        states_value2 = [h2, c2]
+
+        # Generate first input: Start vector.
+        target_seq = np.zeros((1, VEC_LENGTH))
+        target_seq[0, 0] = 1  # first element is "1" to indicate "start"
+
+        # Sampling loop for a batch of sequences
+        # (to simplify, here we assume a batch of size 1).
+        stop_condition = False
+        output_sequence = []
+        step = 0
+        #while step < OUTPUT_TIMESTEPS:
+        total_time = 0
+        while step < MAX_OUTPUT_STEPS and total_time < OUTPUT_BEATS * QUANTIZATION:
+            z = decoder_model.predict(
+                [np.expand_dims(target_seq, 0)] + states_value1 + states_value2)
+            out_vec1, out_vec2, out_vec3, h1, c1, h2, c2 = z
+
+            sampled_command = np.argmax(out_vec1[0, 0, :])
+            sampled_midi = np.argmax(out_vec2[0, 0, :])
+            sampled_dur = np.argmax(out_vec3[0, 0, :])
+
+            # print(sampled_command, sampled_midi, sampled_dur)
+            output_sequence.append((sampled_command, sampled_midi, sampled_dur))
+            step += 1
+            total_time += sampled_dur
+
+            # Exit condition: either hit max length
+            # or find stop character.
+            # if (sampled_word == '</S>' or step > max_output_seq_len):
+            #    stop_condition = True
+
+            # Update the target sequence (of length 1).
+
+            target_seq = np.zeros((1, VEC_LENGTH))
+            target_seq[0, sampled_command] = 1
+            target_seq[0, MIDI_VEC_RANGE[0] + sampled_midi] = 1
+            target_seq[0, DURATION_VEC_RANGE[0] + sampled_dur] = 1
+
+            # Update states
+            states_value1 = [h1, c1]
+            states_value2 = [h2, c2]
+
+        return output_sequence
+
+    return partial(seq2seq_from_models, encoder_model1, encoder_model2,
+                   decoder_model)
+
+
+# Convert Output to CSV and MIDI
+
+def midi_to_mnn(midi, flat_key=True):
+    octave = 3 + midi // 12
+    pc = midi % 12
+    pc_to_degree = pc_to_degree_flat_key if flat_key else pc_to_degree_sharp_key
+    degree = pc_to_degree[pc]
+    return octave * 7 + degree + 4
+
+
+def seq_to_tuples(seq, start_time=100, channel=0, flat_key=True):
+    t = start_time  # time in beats, integer
+    subbeat = 0  # curent subbeat in beat for t, range is 0 to QUANTIZATION-1
+
+    notes = []
+
+    cur_note_start = 0
+    cur_note = None
+    cur_dur = None
+
+    for command, midi, dur in seq:
+        mnn = midi_to_mnn(midi, flat_key)
+        # Time-shift
+        if command == 3:
+            # Record note/rest start data.
+            cur_dur = dur
+            cur_note_start = round(t + subbeat / QUANTIZATION, SIG_FIGS)
+
+            # Update current time.
+            subbeat += dur
+            if subbeat > QUANTIZATION:
+                subbeat = dur % QUANTIZATION
+                t += dur // QUANTIZATION + 1
+
+        # Note on.
+        elif command == 2:
+            if cur_note:
+                notes.append((cur_note_start, midi, mnn,
+                              round(cur_dur / QUANTIZATION, SIG_FIGS),
+                              channel))
+            cur_note = midi + MIDI_MIN - 1  # -1 for the 0 case
+
+        # Note off.
+        elif command == 1:
+            if cur_note:
+                notes.append((cur_note_start, midi, mnn,
+                              round(cur_dur / QUANTIZATION, SIG_FIGS),
+                              channel))
+            cur_note = 0
+    return notes
+
+
+def print_tuples(tuples):
+    for t in tuples:
+        print(t)
+
+
+def target_outputs_to_seq(N, target1, target2, target3):
+    return list(zip(np.argmax(target1[N], axis=1),
+                    np.argmax(target2[N], axis=1),
+                    np.argmax(target3[N], axis=1)))
+
+
+def target_outputs_to_tuples(N, target1, target2, target3):
+    return seq_to_tuples(target_outputs_to_seq(N, target1, target2, target3))
+
+
+def seq_to_csv(seq, filename='tst.csv'):
+    with open(filename, 'w') as f:
+        f.writelines(
+            ','.join(str(x) for x in tup) + '\n' for tup in seq_to_tuples(seq))
+
+
+def predict_for_files(seq2seq, input_generator):
+    ex = 0
+    for [x, y], [target1, target2, target3] in validation_generator:
+        for i in range(len(x)):
+            if ex % 100 == 0:
+                print(ex)
+            seq = seq2seq(np.expand_dims(x[i], axis=0))
+            seq_to_csv(seq, filename='outputs/validation_output_%08d.csv' % ex)
+            ex += 1
+
+
+if __name__ == '__main__':
+    print('--------------------------------------------------')
+    print('MIREX 2018: Patterns for Prediction (Eric Nichols)')
+    print('--------------------------------------------------')
+
+    if sys.argv < 3:
+        print('Usage: %s input_folder output_folder' % sys.args[0])
+
+    INPUT_PATH = sys.args[1]
+    OUTPUT_PATH = sys.args[2]
+
+    seq2seq = load_model()
+    predict_for_files(seq2seq, ...)
+
+
